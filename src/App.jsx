78a91f6a4ce9c3a46abe888573be0e56d55fd6ba
Danfusami01 <adamuabubakarfusami@gmail.com>
2025-12-16 11:45:33 +0100

    Add basic registration form to App component
    Introduced a simple registration form UI in App.jsx with fields for first name, surname, last name, email, password, and a human verification checkbox. Also imported the Events component in both App.jsx and main.jsx, though it is not yet used in the rendered output.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Events from '../src/pages/dashboard/Events'
 import './App.css'
+import './pages/dashboard/Events'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+    <div>
+    <label class="register">Register Form</label>
+    <br></br>
+    <input placeholder='First name'></input>
+    <br></br>
+    <input placeholder='Surname'></input>
+    <br></br>
+    <input placeholder='Last name'></input>
+    <br></br>
+    <input placeholder='Email'></input>
+    <br></br>
+    <label>Verify You're human!</label>
+    <input type="checkbox" id="Verify" name="Verify" value="Verify"></input>
+    <br></br>
+    <input placeholder='Password'></input>
+    <br></br>
+    <button>Submit</button>
+    </div>
+
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +48,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
+
 
 export default App
